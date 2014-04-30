82d08c2b452b34b8a2b7ae11a3b9fd5ff7cb5750
aveekdeveloper <aveekdeveloper@gmail.com>
2014-04-30 12:23:23 +0530

    Update main.php

--- main.php
+++ main.php
@@ -13,9 +13,9 @@ include_once('corelogic/_includes.php');
 
 // MySQL
 
-mysql_connect(global_mysql_server, global_mysql_user, global_mysql_password)or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
-mysql_select_db(global_mysql_database)or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
-mysql_set_charset('utf8');
+//mysql_connect(global_mysql_server, global_mysql_user, global_mysql_password)or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
+//mysql_select_db(global_mysql_database)or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
+//mysql_set_charset('utf8');
 
 define('global_mysql_configuration_table', 'phpmyreservation_configuration');
 define('global_mysql_users_table', 'phpmyreservation_users');
