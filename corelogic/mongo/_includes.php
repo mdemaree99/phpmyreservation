c702c45d6d0ae046407a815e0bca4a0ee87a027e
aveekdeveloper <aveekdeveloper@gmail.com>
2014-04-30 16:16:25 +0530

    Update _includes.php

--- corelogic/mongo/_includes.php
+++ corelogic/mongo/_includes.php
@@ -2,7 +2,7 @@
 
 // MongoDB
 $connection = new MongoClient(global_mongodb_database);
-$db = $connection->playgroundreservation;   //playgroundreservation is the name of the database
+$db = $connection->heroku_app24497420;   //playgroundreservation is the name of the database
 
 include_once('playground_functions_mongo.php');
 include_once('userfunctions_mongo.php');
